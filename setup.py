5d1e8495544fc50aed32e52e034c1ced95741088
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2017-01-10 17:39:58 +0100

    version change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='pylemonway',
-    version='0.1.3',
+    version='0.1.4',
     description='Python wrapper for Lemonway DIRECTKITJSON2',
     long_description='Python wrapper for Lemonway DIRECTKITJSON2',
 
