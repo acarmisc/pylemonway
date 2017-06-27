2e62723b141149c3550573f2e467629ea8e7826b
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2017-06-27 10:43:43 +0200

    implicit code setting in exception

--- pylemonway/lexceptions.py
+++ pylemonway/lexceptions.py
@@ -1,7 +1,7 @@
 class LemonwayException(Exception):
-    def __init__(self, value, code):
+    def __init__(self, value, code=None):
         self.value = value
-        self.code = code
+        self.code = code or value.code
     
     def __str__(self):
         return repr(self.value)
